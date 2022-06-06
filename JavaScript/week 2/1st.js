98438599186a99b6c3a196678a99fa1962bee65f
devansh-m12 <20bcs039@iiitdwd.ac.in>
2022-06-06 19:38:04 +0530

    VS

---
+++ JavaScript/week 2/1st.js
@@ -0,0 +1,19 @@
+var a = document.querySelector(".howareyou");
+a.addEventListener('click', function () {
+
+
+    var h1 = document.querySelector("h1");
+    h1.innerHTML = "I'm Ok what happened man?!!"
+    console.log("h1");
+
+});
+
+var b = document.querySelector(".hello");
+b.addEventListener('click', function () {
+
+
+    var h1 = document.querySelector("h1");
+    h1.innerHTML = "Hey how Ya feeling Man??"
+    console.log("h1");
+
+});
